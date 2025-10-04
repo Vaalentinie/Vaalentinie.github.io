5780e806c8fda13562ce7a2544fbaf4dde3d1de9
Valentinie <valentiniecrsantos@icloud.com>
2025-10-04 12:15:02 -0300

    Aula3

---
+++ intervalo.js
@@ -0,0 +1,12 @@
+ setTimeout(function (){
+        document.getElementById("tempo").textContent = "Mudou";
+    }, 5000);
+   var contador = 0;
+    setInterval(function(){
+        contador = contador + 1;
+        if (contador == 10) {
+            alert('O contador chegou ao 10')
+        }
+        document.getElementById("contador-elemento").textContent = contador;
+
+    }, 1000);
